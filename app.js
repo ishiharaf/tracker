b41ad158697b625310fb12cbcfd517923c4e3791
Ishihara Felipe <k.hishihara@gmail.com>
2021-03-19 16:12:46 +0900

    Add app

---
+++ app.js
@@ -0,0 +1,176 @@
+const fs = require("fs")
+
+const getYear = (date) => {
+	return date.getFullYear().toString()
+}
+const getMonth = (date) => {
+	return (date.getMonth() + 1).toString().padStart(2, "0")
+}
+
+const getDate = (date) => {
+	return date.getDate().toString().padStart(2, "0")
+}
+
+const splitLines = (data) => {
+	return data.split(/\r\n|\n\r|\n|\r/)
+}
+
+const filterLine = (line) => {
+	const filter = ["~", "-"]
+	const arr = line.split(" ")
+	return arr.filter(el => !filter.includes(el))
+}
+
+const isSameDay = (start, end) => {
+	if (end > start) {
+		return true
+	}
+	return false
+}
+
+const getWorkDay = (line) => {
+	const day = {
+		workStart: undefined, workEnd: undefined,
+		breakStart: undefined, breakEnd: undefined
+	}
+	const log = filterLine(line)
+	const start = new Date(`${log[0]} ${log[1]}`)
+	const end = new Date(`${log[0]} ${log[log.length - 1]}`)
+
+	if (log.length === 3) {
+		if (!isSameDay(start, end)) {
+			end.setDate(end.getDate() + 1)
+		}
+	}
+	if (log.length === 5) {
+		const breakStart = new Date(`${log[0]} ${log[2]}`)
+		const breakEnd = new Date(`${log[0]} ${log[3]}`)
+
+		if (!isSameDay(start, breakStart)) {
+			breakStart.setDate(breakStart.getDate() + 1)
+		}
+		if (!isSameDay(breakStart, breakEnd)) {
+			breakEnd.setDate(breakEnd.getDate() + 1)
+		}
+		if (!isSameDay(breakEnd, end)) {
+			end.setDate(end.getDate() + 1)
+		}
+		day.breakStart = breakStart, day.breakEnd = breakEnd
+	}
+	day.workStart = start, day.workEnd = end
+	return day
+}
+
+const getHours = (time) => {
+	const hour = Math.floor(time / 60).toString().padStart(2, "0")
+	const minute = Math.floor(time % 60).toString().padStart(2, "0")
+	return `${hour}:${minute}`
+}
+
+const getBillableTime = (day) => {
+	const billable = ((day.workEnd - day.workStart) / 1000) / 60
+	let work = billable
+
+	if (day.breakStart) {
+		const unbillable = ((day.breakEnd - day.breakStart) / 1000) / 60
+		work = work - unbillable
+	}
+	return work
+}
+
+const getTotalTime = (day) => {
+	return ((day.workEnd - day.workStart) / 1000) / 60
+}
+
+const writeFile = (file, data) => {
+	fs.writeFile(file, data, err => {
+		if (err) return console.error(err)
+	})
+}
+
+const getBillTotal = (billable, rate) => {
+	return ((Math.floor(billable) / 60) * rate).toFixed(2)
+}
+
+const writeInvoice = (file, info) => {
+	const log = `${file.slice(0, 7)}.invoice`
+	const contractor = fs.readFileSync("contractor.info").toString()
+	const company = fs.readFileSync("company.info").toString()
+	const rate = 16
+
+	const now = new Date()
+	let header = `INVOICE\n` +
+				`Date: ${getYear(now)}/${getMonth(now)}/${getDate(now)}\n` +
+				`Invoice number: ${getYear(now)}${getMonth(now)}${getDate(now)}\n\n` +
+				`${contractor}\n\n` +
+				`BILLED TO\n` +
+				`${company}\n\n` +
+				`${info.log}\n` +
+				`  BILLABLE = ${getHours(info.billable)}\n` +
+				`UNBILLABLE = ${getHours(info.unbillable)}\n` +
+				`      RATE = $${rate}\n` +
+				`     TOTAL = $${getBillTotal(info.billable, rate)}`
+	writeFile(log, header)
+}
+
+const billWork = (file) => {
+	fs.readFile(file, "utf-8", (err, data) => {
+		if (err) return console.error(`Couldn't read ${file}`)
+
+		let totalBillable = 0, totalUnbillable = 0, days = 0, log = ""
+		const lines = splitLines(data)
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i]
+			if (line.charAt(0) !== "#") {
+				const day = getWorkDay(line)
+				const billable = getBillableTime(day)
+				const unbillable = getTotalTime(day)
+				const workHours = getHours(billable)
+				const date = line.slice(0, 10)
+				const hours = line.slice(11)
+
+				totalBillable += billable, totalUnbillable += unbillable
+				days += 1, log += `${date} = ${workHours} -> ${hours}\n`
+			}
+		}
+		const info = {
+			billable: totalBillable, unbillable: totalUnbillable,
+			days: days, log: log
+		}
+		writeInvoice(file, info)
+	})
+}
+
+const isDate = (date) => {
+	const timestamp = Date.parse(date)
+	if(!isNaN(timestamp)) {
+		return true
+	}
+	return false
+}
+
+const isLog = (arg) => {
+	if (arg.includes(".log")) {
+		return true
+	}
+	if (isDate(arg)) {
+		return true
+	}
+	return false
+}
+
+const getFile = (date) => {
+	return `${getYear(date)}-${getMonth(date)}.log`
+}
+
+const args = process.argv.slice(2)
+const billArg = args.indexOf("-b") > -1 ? args.indexOf("-b") : undefined
+const logArg = args.length > 0 ? args.length - 1 : undefined
+
+if (billArg >= 0) {
+	if (logArg && isLog(args[logArg])) {
+		billWork(args[logArg])
+	} else {
+		billWork(getFile(new Date()))
+	}
+}
